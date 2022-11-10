0f13379c1b515d03bd86d2f2c516193972c179cd
fpxwe85213-admin <fpxwe85213@yahoo.co.jp>
2022-11-10 21:16:24 +0900

    test

--- react-portfolio-site/src/App.js
+++ react-portfolio-site/src/App.js
@@ -1,25 +1,52 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+const NameText = (props) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <p>Hi, {props.name}!</p>
+  )
 }
+
+const Profile = (props) => {
+  return (
+    <div>
+      <p>{props.age}</p>
+      <p>{props.country}</p>
+    </div>
+  )
+}
+
+const App = () => {
+  return (
+    <div>
+      <NameText name="Jack" />
+      <Profile age={22} country="US" />
+      <NameText name="Mary" />
+      <Profile age={20} country="UK" />
+    </div>
+  )
+}
+
 
 export default App;
