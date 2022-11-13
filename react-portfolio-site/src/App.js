259f8726c8287687a1c5b4d3f38a13efc0261bbd
fpxwe85213-admin <fpxwe85213@yahoo.co.jp>
2022-11-13 22:00:34 +0900

    ポートフォリオ作成

--- react-portfolio-site/src/App.js
+++ react-portfolio-site/src/App.js
@@ -1,52 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+// Headerコンポーネントをimportする
+import { Header } from './components/Header';
+import { Skills } from './components/Skills';
+import { Contact } from './components/Contact';
 
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
-
-const NameText = (props) => {
+function App() {
   return (
-    <p>Hi, {props.name}!</p>
-  )
+    <>
+      <Header />
+      <Skills />
+      <Contact />
+    </>
+  );
 }
-
-const Profile = (props) => {
-  return (
-    <div>
-      <p>{props.age}</p>
-      <p>{props.country}</p>
-    </div>
-  )
-}
-
-const App = () => {
-  return (
-    <div>
-      <NameText name="Jack" />
-      <Profile age={22} country="US" />
-      <NameText name="Mary" />
-      <Profile age={20} country="UK" />
-    </div>
-  )
-}
-
 
 export default App;
---
+++ react-portfolio-site/src/components/Skills.jsx
@@ -0,0 +1,74 @@
+import Circle from "react-circle";
+import { useEffect, useReducer } from "react";
+import axios from "axios";
+import { skillReduser, initialState, actionTypes } from "../reducers/skillReducer";
+import { requestStates } from "../constants";
+
+export const Skills = () => {
+  const [state, dispatch] = useReducer(skillReduser, initialState);
+
+  useEffect(() => {
+    dispatch({ type: actionTypes.fetch });
+    axios.get('https://api.github.com/users/ykmGit/repos')
+      .then((response) => {
+        const languageList = response.data.map(res => res.language);
+        const countedLanguageList = generateLanguageCountObj(languageList);
+        dispatch({ type: actionTypes.success, payload: { languageList: countedLanguageList } });
+      })
+      .catch(() => {
+        dispatch({ type: actionTypes.error });
+      });
+  }, []);
+
+  const generateLanguageCountObj = (allLanguageList) => {
+    const notNullLanguageList = allLanguageList.filter(language => language != null);
+    const uniqueLanguageList = [...new Set(notNullLanguageList)];
+
+    return uniqueLanguageList.map(item => {
+      return {
+        language: item,
+        count: allLanguageList.filter(language => language === item).length
+      }
+    });
+  };
+
+  const converseCountToPercentage = (count) => {
+    if (count > 10) { return 100; }
+    return count * 10;
+  };
+
+  return (
+    <div id="skills">
+      <div className="container">
+        <div className="heading">
+          <h2>Skills</h2>
+        </div>
+        <div className="skills-container">
+          {
+            state.requestStates === requestStates.loading && (
+              <p className="description">取得中...</p>
+            )
+          }
+          {
+            state.requestState === requestStates.success && (
+              state.languageList.map((item, index) => (
+                <div className="skill-item" key={index}>
+                  <p className="description"><strong>{item.language}</strong></p>
+                  <Circle
+                    animate
+                    progress={converseCountToPercentage(item.count)}
+                  />
+                </div>
+              ))
+            )
+          }
+          {
+            state.requestStates === requestStates.error && (
+              <p className="description">エラーが発生しました</p>
+            )
+          }
+        </div>
+      </div>
+    </div>
+  );
+};
